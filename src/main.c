362565479dea089da6cf8983baf06664d84aaa7b
vdolfinn <vdolfinn@imagine.net>
2026-03-30 16:47:55 +0000

    substitute of mock tracks names with actual files scan

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
+#include <dirent.h>
 #include <ncurses.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int PL_WIDTH = 30;
 int PL_HEIGHT = 10;
@@ -11,18 +14,19 @@ void draw_playlist(WINDOW *pl_win, char *tracks[], int tracks_num,
                    int cur_track_idx);
 void draw_cur_track(WINDOW *win, char *cur_track_name);
 
+char **scan_tracks(const char *path, const int page_size, int *tracks_count);
+
 int main() {
   WINDOW *pl_win;
   WINDOW *cur_track_win;
   int cur_hl_track_idx = 0;
   int cur_pl_track_idx = -1;
 
-  char *tracks[] = {
-      "Tepsen.mp3",
-      "Chistiy.mp3",
-      "NeverFadeAway.mp3",
-  };
-  int tracks_num = sizeof(tracks) / sizeof(char *);
+  int tracks_count;
+  char **tracks = scan_tracks("assets", 10, &tracks_count);
+  if (!tracks) {
+    return -1;
+  }
 
   initscr();
   cbreak();
@@ -46,14 +50,14 @@ int main() {
   pl_win = newwin(PL_HEIGHT, PL_WIDTH, pl_start_y, pl_start_x);
 
   draw_cur_track(cur_track_win, NULL);
-  draw_playlist(pl_win, tracks, tracks_num, cur_hl_track_idx);
+  draw_playlist(pl_win, tracks, tracks_count, cur_hl_track_idx);
 
   bool is_terminated = false;
   while (!is_terminated) {
     int ch = getch();
     switch (ch) {
     case 'j':
-      if (cur_hl_track_idx + 1 < tracks_num) {
+      if (cur_hl_track_idx + 1 < tracks_count) {
         cur_hl_track_idx++;
       }
 
@@ -77,10 +81,15 @@ int main() {
     if (cur_pl_track_idx > -1) {
       draw_cur_track(cur_track_win, tracks[cur_pl_track_idx]);
     }
-    draw_playlist(pl_win, tracks, tracks_num, cur_hl_track_idx);
+    draw_playlist(pl_win, tracks, tracks_count, cur_hl_track_idx);
   }
 
   endwin();
+
+  for (int i = 0; i < tracks_count; i++) {
+    free(tracks[i]);
+  }
+  free(tracks);
 
   return 0;
 }
@@ -119,4 +128,47 @@ void draw_cur_track(WINDOW *win, char *cur_track_name) {
   }
 
   wrefresh(win);
+}
+
+char *MP3_EXT = ".mp3";
+
+char **scan_tracks(const char *path, const int page_size, int *tracks_count) {
+  DIR *dir = opendir(path);
+  if (!dir) {
+    perror("opendir");
+    *tracks_count = 0;
+    return NULL;
+  }
+
+  int n = 0;
+  struct dirent *entry;
+  while (n < page_size && (entry = readdir(dir)) != NULL) {
+    char *dot = strchr(entry->d_name, '.');
+    if (dot && strcmp(dot, MP3_EXT) == 0) {
+      n++;
+    }
+  }
+
+  char **track_names = malloc(n * sizeof(char *));
+  if (!track_names) {
+    closedir(dir);
+    perror("malloc track_names");
+    *tracks_count = 0;
+    return NULL;
+  }
+
+  rewinddir(dir);
+  int i = 0;
+  while (n < page_size && (entry = readdir(dir)) != NULL) {
+    char *dot = strchr(entry->d_name, '.');
+    if (dot && strcmp(dot, MP3_EXT) == 0) {
+      int len = dot - entry->d_name;
+      track_names[i] = malloc(len + 1); // +1 for null terminator
+      strncpy(track_names[i], entry->d_name, dot - entry->d_name);
+      i++;
+    }
+  }
+
+  *tracks_count = i;
+  return track_names;
 }
